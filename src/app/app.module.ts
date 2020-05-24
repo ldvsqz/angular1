4f495b58532e7213b526a5d3e76212dc8ee04135
ldvsqz <ldvas24@gmail.com>
2020-05-23 19:16:38 -0600

    asdasdasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { AdminMenuModule } from './admin-menu/admin-menu.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, LoginModule, AdminMenuModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
